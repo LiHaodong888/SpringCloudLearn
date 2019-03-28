8ad31f8f2eea6d4289c0f9913a0aa1017bc4397d
小东 <34288907+LiHaodong888@users.noreply.github.com>
2019-03-28 23:59:18 +0800

    新增Hystrix配置
    新增Hystrix配置

--- ribbon-server/src/main/java/com/li/ribbonserver/RibbonServerApplication.java
+++ ribbon-server/src/main/java/com/li/ribbonserver/RibbonServerApplication.java
@@ -1,15 +1,17 @@
 package com.li.ribbonserver;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-
+@EnableHystrix // 开启Hystrix功能
 @RestController
 @EnableEurekaClient
 @SpringBootApplication
@@ -26,12 +28,19 @@ public class RibbonServerApplication {
         return new RestTemplate();
     }
 
+
+    // 发生熔断时 会调用我们自定义的异常方法 则执行fallback
+    @HystrixCommand(fallbackMethod = "helloError")
     @RequestMapping("/hello")
     public String hello(String name) {
 
         // 地址直接写服务名称即可
         String hello = this.restTemplate().getForObject("http://eureka-client/hello?name" + name, String.class);
         return hello;
+    }
+
+    public String helloError(String name) {
+        return "服务器异常,请稍后再试";
     }
 
 }
